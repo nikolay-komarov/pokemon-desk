ba54c8fa445d4159c85a9d014e4fd69280255415
nikolay-komarov <kny.komarov@gmail.com>
2021-05-16 20:34:33 +0300

    добавит компонент Layout и подключит его в HomePage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../../components/header';
+import Layout from '../../components/layout';
 import Button, { ButtonSize, ButtonColor } from '../../components/button';
 
 import s from './home.module.scss';
@@ -8,10 +9,21 @@ import s from './home.module.scss';
 const HomePage = () => (
   <div className={s.root}>
     <Header />
-    HomePage
-    <Button size={ButtonSize.medium} color={ButtonColor.green} isFullWidth={false} onClick={() => console.log('click')}>
-      See Pokemons
-    </Button>
+    <Layout className={s.contentWrap}>
+      <div>
+        <h1>
+          <b>Find</b> all your favorite <b>Pokemon</b>
+        </h1>
+        <p>You can know the type of Pokemon, its strengths, disadvatages and abilities</p>
+        <Button
+          size={ButtonSize.medium}
+          color={ButtonColor.green}
+          isFullWidth={false}
+          onClick={() => console.log('click')}>
+          See Pokemons
+        </Button>
+      </div>
+    </Layout>
   </div>
 );
 
